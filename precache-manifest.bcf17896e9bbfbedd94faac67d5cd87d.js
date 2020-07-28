43f465acc6df67b337cb91d29f3894e9133feb76
Ghanna96 <akramghanna@outlook.com>
2020-07-28 13:04:24 +0200

    Updates

---
+++ precache-manifest.bcf17896e9bbfbedd94faac67d5cd87d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8d14632013cf91b674763812f03b518",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "0159d6c3c8dde2686c2b",
+    "url": "/battleship/static/js/2.4327d328.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/battleship/static/js/2.4327d328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af11ad673dd9081d2dc0",
+    "url": "/battleship/static/js/main.6a65791e.chunk.js"
+  },
+  {
+    "revision": "1576b120f8ea920fbd40",
+    "url": "/battleship/static/js/runtime-main.9e463cc9.js"
+  }
+]);
